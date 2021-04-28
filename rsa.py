6a854260a5a88abb881c88b0b19f3f5e92d1277d
Dani <54872701+danielleappel@users.noreply.github.com>
2021-04-27 21:14:49 -0400

    added generate keys

--- rsa.py
+++ rsa.py
@@ -30,9 +30,63 @@ def decrypt(c, pk):
     message = [chr(ord(char)**d % n) for char in c]
     return "".join(message)
 
-pub_key = [3233,17]
-priv_key = [3233, 2753]
+def generate_keys(minimum):
+    p, q = get_prime(minimum), get_prime(minimum)
+    n = p * q
+    phi = (p - 1) * (q - 1)
 
-print(encrypt("Hello",pub_key))
+    e = get_coprime(phi, minimum)
+    print(e)
 
-print(decrypt("ஸԡ˩˩ࢉ",priv_key))
+    d = multiplicative_inverse(e, phi)
+
+    return (n,e), (n,d)
+
+def gcd(a, b):
+    while b != 0:
+        (a,b) = (b, a % b)
+    return a
+
+def gcd_extended(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = gcd_extended(b % a, a)
+        return (g, x - b//a * y, y)
+
+def multiplicative_inverse(a, n):
+    g, x, y = gcd_extended(a, n)
+    if g != 1:
+        raise Exception("Modular inverse does not exist")
+    else:
+        return x % n
+
+def get_prime(minimum):
+    p = random.randint(minimum, 10*minimum)
+    while not is_prime(p):
+        p = random.randint(minimum, 10*minimum)
+    return p
+
+def get_coprime(phi, minimum):
+    e = random.randint(minimum, 10*minimum)
+    while gcd(phi, e) != 1:
+        e = random.randint(minimum, 10*minimum)
+    return e
+
+#pub_key, priv_key = generate_keys(100)
+#print(pub_key,priv_key)
+
+#pub_key = [3233,17]
+#priv_key = [3233, 2753]
+
+#print(encrypt("Hello",pub_key))
+#print(decrypt("ஸԡ˩˩ࢉ",priv_key))
+
+#pub_key = [175187,5]
+#priv_key = [175187, 139373]
+
+pub_key = [286271, 781]
+priv_key = [286271, 240277]
+
+print(encrypt("Purple Pimento",pub_key))
+print(decrypt("𳶦񅯧𤟺𼻬𼌍𼃥𰚏𳶦𑋂𾄎𼃥𙊨𞅲񄗩",priv_key))
